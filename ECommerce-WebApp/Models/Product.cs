f583927f1f691668d9e15ed45a0b13a2fcd41d87
rushi24-patil <rushikeshpatil2402@gmail.com>
2026-01-18 23:27:26 +0530

    Created Product Controller, View and Model

---
+++ ECommerce-WebApp/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ECommerce_WebApp.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        public string ImageUrl { get; set; } = "/images/default-product.png";
+
+        public int StockQuantity { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public int BrandId { get; set; }
+
+    }
+}
