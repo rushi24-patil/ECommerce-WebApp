9a5b598fa189724668863b8f60f34c54232840a0
rushi24-patil <rushikeshpatil2402@gmail.com>
2026-01-21 22:44:54 +0530

    Implemented View for Products and their details.

--- ECommerce-WebApp/Controllers/ProductController.cs
+++ ECommerce-WebApp/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using ECommerce_WebApp.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ECommerce_WebApp.Controllers
 {
@@ -12,7 +13,31 @@ namespace ECommerce_WebApp.Controllers
         }
         public IActionResult Index()
         {
-            return View();
+            var products = _context.Products
+                .Include(p => p.Category)
+                .Include(p => p.Brand)
+                .ToList();
+
+            ViewData["Categories"] = _context.Categories.ToList();
+            ViewData["Brands"] = _context.Brands.ToList();
+
+            return View(products);
         }
+        public IActionResult Details(int id)
+        {
+            var product = _context.Products
+                .Include(p => p.Category)
+                .Include(p => p.Brand)
+                .FirstOrDefault(p => p.Id == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+
     }
 }
