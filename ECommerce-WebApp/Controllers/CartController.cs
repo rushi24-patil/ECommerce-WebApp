7fed6a6ad96be7071821e9e57650ef320ce7b76b
rushi24-patil <rushikeshpatil2402@gmail.com>
2026-01-21 23:07:47 +0530

    Creating Cart controller and View

---
+++ ECommerce-WebApp/Controllers/CartController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce_WebApp.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task< IActionResult> AddToCart(int ProdutId)
+        {
+            return null;
+        }
+    }
+}
